468f14a18f0c5a84b1ac11325b4e3d941d2daae0
MaayanShiran <maayanishiran@gmail.com>
2024-07-21 14:14:38 +0300

    3

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,14 +3,3 @@ plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.android.library) apply false
 }
-
-buildscript {
-    dependencies {
-        classpath("com.android.tools.build:gradle:8.5.0")  // Update to your AGP version
-        // Add other dependencies as needed
-    }
-}
-
-tasks.register<Delete>("clean") {
-    delete(rootProject.buildDir)
-}
